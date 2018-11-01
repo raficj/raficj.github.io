c87d4a893836486b5a5b458a02d86e6c1f5942bd
Rafic Jouejati <rjouejati@gmail.com>
2018-11-01 13:22:02 -0400

    add new title

--- script.js
+++ script.js
@@ -1,58 +1,31 @@
+$(document).ready(function(){
+  // elements appear on scroll
+  ScrollReveal().reveal('.headline');
+  ScrollReveal().reveal('.tagline');
 
-  // jQuery is required to run this code
-  $( document ).ready(function() {
-    scaleVideoContainer();
+  
+  var scrollLink = $('.scroll');
+  // smooth scroll
+  scrollLink.click(function(e){
+    e.preventDefault();
+    $('body, html').animate({
+        scrollTop: $(this.hash).offset().top
+      });
+  })
 
-    initBannerVideoSize('.video-container .poster img');
-    initBannerVideoSize('.video-container .filter');
-    initBannerVideoSize('.video-container video');
+  // active link switching
+  $(window).scroll(function(){
+    var scrollbarLocation = $(this).scrollTop();
 
-    $(window).on('resize', function() {
-      scaleVideoContainer();
-      scaleBannerVideoSize('.video-container .poster img');
-      scaleBannerVideoSize('.video-container .filter');
-      scaleBannerVideoSize('.video-container video');
+    scrollLink.each(function(){
+
+      var sectionOffset = $(this.hash).offset().top - 10;
+
+      if(sectionOffset <= scrollbarLocation){
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+      }
     });
   });
+})
 
-  function scaleVideoContainer() {
-    var height = $(window).height() + 5;
-    var unitHeight = parseInt(height) + 'px';
-    $('.homepage-hero-module').css('height',unitHeight);
-  }
-
-  function initBannerVideoSize(element){
-    $(element).each(function(){
-      $(this).data('height', $(this).height());
-      $(this).data('width', $(this).width());
-    });
-
-    scaleBannerVideoSize(element);
-  }
-
-  function scaleBannerVideoSize(element) {
-
-    var windowWidth = $(window).width(),
-    windowHeight = $(window).height() + 5,
-    videoWidth,
-    videoHeight;
-
-    // console.log(windowHeight);
-
-    $(element).each(function(){
-      var videoAspectRatio = $(this).data('height')/$(this).data('width');
-
-      $(this).width(windowWidth);
-
-      if(windowWidth < 1000){
-          videoHeight = windowHeight;
-          videoWidth = videoHeight / videoAspectRatio;
-          $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
-
-          $(this).width(videoWidth).height(videoHeight);
-      }
-
-      $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-
-    });
-  }
