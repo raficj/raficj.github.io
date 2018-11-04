6bd713820c14b0bbd146553f958847f9ea6d332b
Rafic Jouejati <rjouejati@gmail.com>
2018-11-03 22:20:04 -0400

    add contact.php

--- contact.php
+++ contact.php
@@ -1,20 +1,6 @@
 <?php
 
 if($_POST["submit"]) {
-    $recipient="your@email.address";
-    $subject="Form to email message";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
-    $message=$_POST["message"];
-
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
-}
-
-if($_POST["submit"]) {
     $recipient="rjouejati@gmail.com";
     $subject="Rafic's Portfolio Inquiry";
     $name=$_POST["name"];
