3aaa27acaf4d6a256e60b332637220e887e75599
Rafic Jouejati <rjouejati@gmail.com>
2018-11-04 02:49:06 -0500

    add contact form functionality

--- script.js
+++ script.js
@@ -2,6 +2,47 @@ $(document).ready(function(){
 
   var navbar = document.querySelector('.navbar');
   var logo = document.querySelector('.logo');
+  // get the form
+  var form = $('#ajax-contact');
+  // get the messages div
+  var formMessages = $('#form-messages');
+
+  $(form).submit(function(e){
+    e.preventDefault();
+    var formData = $(form).serialize();
+
+    // submit the form using AJAX
+    $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'),
+      data: formData
+    })
+    .done(function(response) {
+        // Make sure that the formMessages div has the 'success' class.
+        $(formMessages).removeClass('error');
+        $(formMessages).addClass('success');
+
+        // Set the message text.
+        $(formMessages).text(response);
+
+        // Clear the form.
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+    })
+    .fail(function(data) {
+        // Make sure that the formMessages div has the 'error' class.
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+
+        // Set the message text.
+        if (data.responseText !== '') {
+            $(formMessages).text(data.responseText);
+        } else {
+            $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        }
+    });
+  }
   // elements appear on scroll
   ScrollReveal().reveal('.headline');
   ScrollReveal().reveal('.tagline', {delay:500});
@@ -45,7 +86,9 @@ $(document).ready(function(){
     }
   }
 
-})
+  
+
+});
 
 
 
