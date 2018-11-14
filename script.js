80029a98ecaf355dc0443e04e6edcfc8c893f874
Rafic Jouejati <rjouejati@gmail.com>
2018-11-14 17:32:06 -0500

    add new stuff

--- script.js
+++ script.js
@@ -11,44 +11,6 @@ $(document).ready(function(){
   ScrollReveal().reveal('.headline');
   ScrollReveal().reveal('.tagline', {delay:500});
 
-  // $(form).submit(function(e){
-  //   e.preventDefault();
-  //   var formData = $(form).serialize();
-
-  //   // submit the form using AJAX
-  //   $.ajax({
-  //     type: 'POST',
-  //     url: $(form).attr('action'),
-  //     data: formData
-  //   })
-  //   .done(function(response) {
-  //       // Make sure that the formMessages div has the 'success' class.
-  //       $(formMessages).removeClass('error');
-  //       $(formMessages).addClass('success');
-
-  //       // Set the message text.
-  //       $(formMessages).text(response);
-
-  //       // Clear the form.
-  //       $('#name').val('');
-  //       $('#email').val('');
-  //       $('#message').val('');
-  //   })
-  //   .fail(function(data) {
-  //       // Make sure that the formMessages div has the 'error' class.
-  //       $(formMessages).removeClass('success');
-  //       $(formMessages).addClass('error');
-
-  //       // Set the message text.
-  //       if (data.responseText !== '') {
-  //           $(formMessages).text(data.responseText);
-  //       } else {
-  //           $(formMessages).text('Oops! An error occured and your message could not be sent.');
-  //       }
-  //   });
-  // }
-
-
   
   var scrollLink = $('.scroll');
   // smooth scroll
@@ -75,8 +37,8 @@ $(document).ready(function(){
   });
 
        window.onscroll = function(){
-  scrollFunction();
-};
+        scrollFunction();
+        };
 
   function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
